31e303d5bb5cd13f7d0131544d6006321688112c
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-07-31 14:12:13 +0530

    created netflix like app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Card from "./Card";
+import "./index.css";
+import sdata from "./sdata";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(sdata);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+        <h1 className="heading_stryle">List of top 5 Netflix Series in 2022</h1>
+        <Card imgsrc={sdata[0].imgsrc} title={sdata[0].title} sname={sdata[0].sname} link={sdata[0].link} />
+        <Card imgsrc={sdata[1].imgsrc} title={sdata[1].title} sname={sdata[1].sname} link={sdata[1].link} />
+        <Card imgsrc={sdata[2].imgsrc} title={sdata[2].title} sname={sdata[2].sname} link={sdata[2].link} />
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ src/sdata.js
@@ -0,0 +1,34 @@
+let sdata = [
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Original Series",
+        sname: "DARK",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp9882014.jpg",
+        title: "A Netflix Original Series",
+        sname: "Extra Curricular",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp1917154.jpg",
+        title: "A Netflix Original Series",
+        sname: "Stranger Things",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Original Series",
+        sname: "DARK",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Original Series",
+        sname: "DARK",
+        link: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+];
+
+export default sdata;
